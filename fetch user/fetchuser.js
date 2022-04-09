1e5c868498b2d52c2af0b69a23d9910b9b18002c
arbaaz <arbaazuniquegzp@gmail.com>
2022-04-09 03:21:07 -0700

    fetch user uadted

--- fetch user/fetchuser.js
+++ fetch user/fetchuser.js
@@ -1,10 +1,12 @@
 const express=require('express');
+const authenticate = require('../authentication/auth');
 const app=express();
 const User=require('../models/model');
 
-const getuser=app.get('/getuser',(req,res)=>{
+const getuser=app.get('/getuser',authenticate,(req,res)=>{
     const getUser= async ()=>{
-       const result = await User.find();
+        const userid=req.user;
+       const result = await User.find({_id:userid});
        res.json(result);
     }
     getUser();
